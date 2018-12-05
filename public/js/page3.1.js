5c95201b70bc47c1daa5bd25d92a138e7498d5fb
DglyP <lt4admin@sbrunswilt4.ecn.purdue.edu>
2018-12-05 04:08:37 -0500

    Demo

---
+++ public/js/page3.1.js
@@ -0,0 +1,13 @@
+var cnt = document.getElementById("count"); 
+var water = document.getElementById("water");
+var percent = cnt.innerText;
+var interval;
+interval=setInterval(function(){ 
+  percent++; 
+  cnt.innerHTML = percent; 
+  water.style.transform='translate(0'+','+(100-percent)+'%)';
+  if(percent==100){
+    clearInterval(interval);
+    window.location.href = "page4.html";
+  }
+},60);
