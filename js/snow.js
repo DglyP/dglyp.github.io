6f0ada6784c01c281776218c606a136c9b6c998f
Degly Pava <DglyP@users.noreply.github.com>
2022-01-30 11:35:21 +0100

    Fixed CV

--- js/snow.js
+++ js/snow.js
@@ -66,7 +66,8 @@ function init() {
 }
 
 function onWindowResize( event ) {
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  // renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(container.clientWidth, container.clientHeight);
   uniforms.u_resolution.value.x = renderer.domElement.width;
   uniforms.u_resolution.value.y = renderer.domElement.height;
 }
